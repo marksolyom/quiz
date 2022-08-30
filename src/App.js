50086c2ea5a023d5b5b5c0b8f972f24ae7a00d51
marksolyom <solyom.mark@gmail.com>
2022-08-30 11:51:04 +0200

    create option select logic and correct answer state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Question from "./components/Question"
 export default function App() {
   const [gameStarted, setGameStarted] = useState(false);
   const [quizData, setQuizData] = useState([]);
+  const [correctAnswers, setCorrectAnswers] = useState([]);
+  const [selectedOptions, setSelectedOptions] = useState([]);
 
   useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=5&category=15&type=multiple")
@@ -14,10 +16,40 @@ export default function App() {
   }, [])
 
   function startGame() {
-    setGameStarted(!gameStarted)
+    setGameStarted(!gameStarted);
+
+    const newCorrect = [];
+    quizData.forEach(question => {
+      newCorrect.push({
+        id: question.question,
+        value: question.value
+      })
+    })
+    setCorrectAnswers(newCorrect);
+
+    const newSelection = [];
+    quizData.forEach(question => {
+      newSelection.push({
+        id: question.question,
+        value: ""
+      })
+    })
+    setSelectedOptions(newSelection);
   }
 
-  console.log(quizData)
+  function handleAnswerChange(event) {
+    const { id, value } = event.target;
+    console.log(id, value)
+    console.log(selectedOptions)
+    
+    const newSelection = selectedOptions.map(question => {
+      if (question.id === id) {
+        return { ...question, value: value };
+      }
+      return question;
+    })
+    setSelectedOptions(newSelection);
+  }
 
   const quiz = quizData.map(item => {
     return (
@@ -26,11 +58,13 @@ export default function App() {
         question={item.question}
         wrongOptions={item.incorrect_answers}
         correctOption={item.correct_answer}
+        selectedOptions={selectedOptions}
+        handleChange={handleAnswerChange}
       />
     )
   })
 
-  console.log(quiz)
+  console.log(selectedOptions);
 
   return (
     <div className="App">
--- src/components/Question.js
+++ src/components/Question.js
@@ -2,18 +2,16 @@ import { shuffle } from "lodash";
 import { nanoid } from 'nanoid'
 
 export default function Question(props) {
-    console.log(props)
     const shuffledAnswers = shuffle([...props.wrongOptions, props.correctOption]);
-    console.log(shuffledAnswers);
-
     const options = shuffledAnswers.map(answer => {
         return (
             <div key={nanoid()}>
                 <input
-                    id={answer}
-                    name={answer}
+                    id={props.question}
+                    name={props.question}
                     value={answer}
                     type="radio"
+                    onChange={props.handleChange}
                 />
                 <label
                     htmlFor={answer}>
@@ -24,7 +22,7 @@ export default function Question(props) {
     })
 
     return (
-        <div className="question">
+        <div className="quiz">
             <p>{props.question}</p>
             <div className="options">
             {options}
